c9d8f7dcf7d87357b14bf71dee8c167a772697de
ameltzer <ameltzer91@gmail.com>
2020-03-14 18:43:41 -0400

    Revert "add missing dependencies"
    This reverts commit a818f974c2ed7f0b4710aea2c5565b47c2a72b28.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devtool: "source-map",
 
   resolve: {
-    extensions: [".ts", ".tsx", ".mjs", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
 
   module: {
