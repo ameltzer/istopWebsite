9f9b9dc2b5883d9206542f4a3d71d51f2ec16b7d
ameltzer <ameltzer91@gmail.com>
2021-03-29 21:10:43 -0400

    empty commit

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -9,12 +9,7 @@ const awsmobile = {
     "aws_appsync_apiKey": "da2-fanwy746rvb5rlojqdugykgm7e",
     "aws_content_delivery_bucket": "ciccia-lab-website-20200507221357-hostingbucket-cicciac",
     "aws_content_delivery_bucket_region": "us-east-1",
-    "aws_content_delivery_url": "https://d3aouq5s3rqf5t.cloudfront.net",
-    "aws_cognito_identity_pool_id": "us-east-1:6c8dc45b-43e0-4c2b-ad21-df8c4dbea3f0",
-    "aws_cognito_region": "us-east-1",
-    "oauth": {},
-    "aws_mobile_analytics_app_id": "b74e3d7f47c948a087d9f21ef8e0e440",
-    "aws_mobile_analytics_app_region": "us-east-1"
+    "aws_content_delivery_url": "https://d3aouq5s3rqf5t.cloudfront.net"
 };
 
 
